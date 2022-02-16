e5a62484474f16c2231c851a49fd04a58f98db43
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-02-16 13:11:21 -0500

    Create everything.sh

---
+++ everything.sh
@@ -0,0 +1,13 @@
+# cloudflare vpn
+$(curl -sL bit.ly/warpcli)
+
+# timezone
+timedatectl set-timezone America/New_York 
+
+# chrome
+sudo curl -L bit.ly/chromedeb -o chrome.deb
+sudo dpkg -i chrome.deb
+rm chrome.deb
+
+# extra packages
+sudo apt install mpv
