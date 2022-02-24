a2e94d5ba69ed95d3c2194c6afc2530590f1f983
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-02-24 13:35:08 -0500

    Update everything.sh

--- everything.sh
+++ everything.sh
@@ -1,18 +1,20 @@
 # timezone
 timedatectl set-timezone America/New_York 
 
-# chrome
+# install chrome
 sudo curl -L bit.ly/chromedeb -o chrome.deb
 sudo dpkg -i chrome.deb
 rm chrome.deb
 
 # change dns
-nmcli connection modify "Wired connection 1" ipv4.dns "1.1.1.1,1.0.0.1"
+nmcli con mod "Wired connection 1" ipv4.dns "1.1.1.1,1.0.0.1"
+nmcli con mod "Wired connection 1" ipv4.ignore-auto-dns yes
 nmcli connection down "Wired connection 1"
 nmcli connection up "Wired connection 1"
+sleep 1
 
-# cloudflare vpn
-curl -sL bit.ly/warpcli | bash
-
-# extra packages
+# install mpv
 sudo apt install mpv
+
+# install warp-cli
+curl -sL bit.ly/warpcli | bash
