4d3591cfb0251ec26920dce08dcf0654e735bfdc
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-03-04 14:40:32 -0500

    Update everything.sh

--- everything.sh
+++ everything.sh
@@ -7,14 +7,15 @@ sudo dpkg -i chrome.deb
 rm chrome.deb
 
 # change dns
-nmcli con mod "Wired connection 1" ipv4.dns "1.1.1.1,1.0.0.1"
-nmcli con mod "Wired connection 1" ipv4.ignore-auto-dns yes
-nmcli connection down "Wired connection 1"
-nmcli connection up "Wired connection 1"
+CON="Wired connection 1"
+nmcli con mod "$CON" ipv4.dns "1.1.1.1,1.0.0.1"
+nmcli con mod "$CON" ipv4.ignore-auto-dns yes
+nmcli connection down "$CON"
+nmcli connection up "$CON"
 sleep 1
 
 # install mpv
 sudo apt install mpv
 
 # install warp-cli
-curl -sL bit.ly/warpcli | bash
+curl -sL bit.ly/warpcli
