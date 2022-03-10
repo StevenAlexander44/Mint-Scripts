d965341e256bd691838a97ce4e80c7fcaf563526
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-03-09 20:33:18 -0500

    Update everything.sh

--- everything.sh
+++ everything.sh
@@ -1,11 +1,6 @@
 # timezone
 timedatectl set-timezone America/New_York 
 
-# install chrome
-sudo curl -L bit.ly/chromedeb -o chrome.deb
-sudo dpkg -i chrome.deb
-rm chrome.deb
-
 # change dns
 CON="Wired connection 1"
 nmcli con mod "$CON" ipv4.dns "1.1.1.1,1.0.0.1"
@@ -14,8 +9,13 @@ nmcli connection down "$CON"
 nmcli connection up "$CON"
 sleep 1
 
+# install chrome
+sudo curl -L bit.ly/chromedeb -o chrome.deb
+sudo dpkg -i chrome.deb
+rm chrome.deb
+
 # install mpv
 sudo apt install mpv
 
-# install warp-cli
-curl -sL bit.ly/warpcli | bash
+# install warp
+curl -sL bit.ly/wgcfdl | bash
